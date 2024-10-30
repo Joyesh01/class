7028519b7e06bba5faf8f15e2e13edcc7b1c2f2a
JOYESH <114464409+Joyesh01@users.noreply.github.com>
2024-10-30 11:00:11 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+#define pb push_back;
+const int MAX=1e18;
+const int MIN=-1e18;
+//(f==1)?cout<<"YES\n":cout<<"NO\n";
+
+void solve()
+{
+    int n;
+    cin>>n;
+    
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
